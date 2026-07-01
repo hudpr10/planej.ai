1cc1a87524c76d9e1032aa76341638ccd5acf492
Hudson Reis <hudsonpr09@gmail.com>
2026-07-01 16:30:13 -0300

    :sparkles: add: created input and updated form

--- src/components/features/Simulation/Form/index.tsx
+++ src/components/features/Simulation/Form/index.tsx
@@ -1,5 +1,20 @@
+import { PiggyBank } from 'lucide-react';
+
+import SimulationFormStep from '../FormStep';
+import SimulationProgress from '../Progress';
+
 const SimulationForm = () => {
-  return <form></form>;
+  return (
+    <form>
+      <SimulationProgress currentStep={1} totalSteps={6} />
+      <SimulationFormStep
+        icon={PiggyBank}
+        title="Renda mensal bruta"
+        question="Quanto é depositado na sua conta todo mês (somando todas as fontes)"
+        inputProps={{ prefix: 'R$', type: 'text', placeholder: 'ex: 5.000,00' }}
+      />
+    </form>
+  );
 };
 
 export default SimulationForm;
---
+++ src/components/shared/Input/index.tsx
@@ -0,0 +1,34 @@
+import type { InputHTMLAttributes } from 'react';
+
+import Divider from '../Divider/intex';
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  prefix?: string;
+  suffix?: string;
+}
+
+const Input = ({ prefix, suffix, ...props }: InputProps) => {
+  return (
+    <div className="bg-input flex items-center rounded-2xl p-4 shadow-[4px_4px_18px_0px_rgba(0,0,0,0.2)]">
+      {prefix && (
+        <>
+          <span className="text-muted-foreground text-sm font-medium">{prefix}</span>
+          <Divider orientation="vertical" />
+        </>
+      )}
+      <input
+        className="text-foreground placeholder:text-muted-foreground w-full bg-transparent text-sm outline-none"
+        autoFocus
+        {...props}
+      />
+      {suffix && (
+        <>
+          <Divider orientation="vertical" />
+          <span className="text-muted-foreground text-sm font-medium">{suffix}</span>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Input;
