89584fe7c43ba6b783474273f67bf3b67010dbfc
Hudson Reis <hudsonpr09@gmail.com>
2026-07-01 13:00:50 -0300

    :sparkles: add: progress component and style

--- src/pages/SimulationFormPage/index.tsx
+++ src/pages/SimulationFormPage/index.tsx
@@ -1,11 +1,13 @@
 import SimulationForm from '@/components/features/Simulation/Form';
 import SimulationHero from '@/components/features/Simulation/Hero';
+import SimulationProgress from '@/components/features/Simulation/Progress';
 
 const SimulationFormPage = () => {
   return (
     <main className="mx-auto max-w-xl px-4 py-10 sm:py-14">
       <SimulationHero />
       <SimulationForm />
+      <SimulationProgress currentStep={1} totalSteps={6} />
     </main>
   );
 };
