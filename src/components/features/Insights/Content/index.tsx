673d8d7332a5f41125f6c466dfef4deb35e440af
Hudson Reis <hudsonpr09@gmail.com>
2026-07-07 15:39:23 -0300

    :lipstick: styles: loading and error

---
+++ src/components/features/Insights/Content/index.tsx
@@ -0,0 +1,5 @@
+const Content = () => {
+  return <p>Conteúdo</p>;
+};
+
+export default Content;
--- src/components/features/SimulationResults/AIInsightCard/index.tsx
+++ src/components/features/SimulationResults/AIInsightCard/index.tsx
@@ -1,12 +1,44 @@
+import 'react-loading-skeleton/dist/skeleton.css';
+
+import Skeleton from 'react-loading-skeleton';
+
 import { useInsight } from '@/hooks/useInsight';
 
+import Content from '../../Insights/Content';
+import Error from '../../Insights/Error';
+
 const AIInsightCard = ({ simulationId }: { simulationId: string }) => {
-  const { insight } = useInsight(simulationId);
-  console.log(insight);
+  const { insight, isLoading, error, fetchInsight } = useInsight(simulationId);
 
   return (
     <div className="bg-card order-2 rounded-2xl p-6 shadow-[4px_4px_18px_0px_rgba(0,0,0,0.2)] lg:order-1 lg:col-span-2">
-      Painel de Insights
+      <div className="mb-3 flex items-center gap-1.5">
+        <span>✨</span>
+        <span className="text-primary text-xs font-semibold tracking-widest uppercase">
+          Insight Financeiro Personalizado
+        </span>
+      </div>
+
+      {isLoading && (
+        <div className="flex">
+          <Skeleton
+            count={10.5} // Linhas
+            baseColor="var(--color-skeleton-base)" // Cores
+            highlightColor="var(--color-skeleton-highlight)"
+            className="mb-3 flex rounded-lg" // Classes de estilização
+            containerClassName="flex-1" // Ocupa todo o componente
+            inline // Não quebre nenhum elemento com <br>
+          />
+        </div>
+      )}
+      {!isLoading && error && (
+        <Error
+          simulationId={simulationId}
+          message={error}
+          onRetry={() => fetchInsight(simulationId)}
+        />
+      )}
+      {!isLoading && insight && !error && <Content />}
     </div>
   );
 };
